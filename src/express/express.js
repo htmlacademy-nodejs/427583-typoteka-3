e1ce64fbbdfda7c2b0eafda75272fd05fb5eadb3
RomanBykov <rmnbykov@gmail.com>
2021-04-21 23:35:13 +0300

    Личный проект: по тропе Аппалачей

---
+++ src/express/express.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require(`express`);
+const app = express();
+
+const DEFAULT_PORT = 8080;
+
+const mainRoutes = require(`./routes/main-routes`);
+const myRoutes = require(`./routes/my-routes`);
+const articlesRoutes = require(`./routes/articles-routes`);
+
+app.use(`/`, mainRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/articles`, articlesRoutes);
+
+app.listen(DEFAULT_PORT);
